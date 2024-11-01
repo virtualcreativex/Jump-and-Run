71133944c23d82431bc24a4a99879a7bc1d3a564
Monica Yaneth Loeb Willes <manager@virtualcreativex.com>
2024-10-31 21:27:14 -0500

    Create a spawn manager

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -2,20 +2,22 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MoveLeft : MonoBehaviour
+public class SpawnManager : MonoBehaviour
 {
     
-    public float speed = 10.0f;
+    public GameObject obstaclePrefab;
+    private Vector3 spawnPos = new Vector3(25, 0, 0);
+    
     
     // Start is called before the first frame update
     void Start()
     {
-        
+        Instantiate(obstaclePrefab, spawnPos, obstaclePrefab.transform.rotation);
     }
-
+    
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(Vector3.left  * (Time.deltaTime * speed));
+        
     }
 }
