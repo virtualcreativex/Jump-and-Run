dd0418088bbd40160815139b23a4f6c6cd0be40d
Monica Yaneth Loeb Willes <manager@virtualcreativex.com>
2024-10-31 20:30:25 -0500

    Tweak the jump force and gravity

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,11 +6,14 @@ public class PlayerController : MonoBehaviour
 {
 
     private Rigidbody playerRb;
+    public float jumpForce = 10.0f;
+    public float gravityModifier = 1.5f;
     
     // Start is called before the first frame update
     void Start()
     {
         playerRb = GetComponent<Rigidbody>();
+        Physics.gravity *= gravityModifier;
     }
 
     // Update is called once per frame
@@ -18,7 +21,7 @@ public class PlayerController : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            playerRb.AddForce(Vector3.up * 10f, ForceMode.Impulse);
+            playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
         }    
             
     }
