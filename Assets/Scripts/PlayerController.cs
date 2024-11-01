a8a9a8cd42d18fcfc21a64b6f6947087c8709b2f
Monica Yaneth Loeb Willes <manager@virtualcreativex.com>
2024-10-31 20:40:08 -0500

    Prevent player from double-jumping

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,6 +8,7 @@ public class PlayerController : MonoBehaviour
     private Rigidbody playerRb;
     public float jumpForce = 10.0f;
     public float gravityModifier = 1.5f;
+    public bool isOnGround = true;
     
     // Start is called before the first frame update
     void Start()
@@ -19,10 +20,16 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space) && isOnGround)
         {
             playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            isOnGround = false;
         }    
-            
     }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        isOnGround = true;
+    }
+    
 }
