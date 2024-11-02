47fcd269a21b27876a052fa458956ca8fb5e1125
Monica Yaneth Loeb Willes <manager@virtualcreativex.com>
2024-11-02 17:52:26 -0500

    Set up a falling animation

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -28,7 +28,7 @@ public class PlayerController : MonoBehaviour
         {
             playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
             isOnGround = false;
-            playerAnim.SetTrigger("Jump_trig");
+            playerAnim.SetTrigger("Jump_trig"); 
         }    
     }
 
@@ -42,6 +42,8 @@ public class PlayerController : MonoBehaviour
         {
             Debug.Log("Game Over");
             gameOver = true;
+            playerAnim.SetBool("Death_b",true);
+            playerAnim.SetInteger("DeathType_int", 1);
         }
         
     }
