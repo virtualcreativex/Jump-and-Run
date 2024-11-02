b68bb5b393bfd53932d8158618992fce42fcd319
Monica Yaneth Loeb Willes <manager@virtualcreativex.com>
2024-11-01 21:26:43 -0500

    Add a new game over trigger

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -9,6 +9,7 @@ public class PlayerController : MonoBehaviour
     public float jumpForce = 10.0f;
     public float gravityModifier = 1.0f;
     public bool isOnGround = true;
+    public bool gameOver;  
     
     // Start is called before the first frame update
     void Start()
@@ -29,7 +30,16 @@ public class PlayerController : MonoBehaviour
 
     private void OnCollisionEnter(Collision collision)
     {
-        isOnGround = true;
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isOnGround = true;
+        }
+        else if (collision.gameObject.CompareTag("Obstacle"))
+        {
+            Debug.Log("Game Over");
+            gameOver = true;
+        }
+        
     }
     
 }
