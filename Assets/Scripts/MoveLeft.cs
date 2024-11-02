7d535ae0340494ce0ca105108a4c43044faf9198
Monica Yaneth Loeb Willes <manager@virtualcreativex.com>
2024-11-01 22:25:31 -0500

    Stop MoveLeft on gameOver

--- Assets/Scripts/MoveLeft.cs
+++ Assets/Scripts/MoveLeft.cs
@@ -6,16 +6,20 @@ public class MoveLeft : MonoBehaviour
 {
     
     public float speed = 10.0f;
+    private PlayerController playerControllerScript;
     
     // Start is called before the first frame update
     void Start()
     {
-        
+        playerControllerScript = GameObject.Find("Player").GetComponent<PlayerController>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(Vector3.left  * (Time.deltaTime * speed));
+        if (playerControllerScript.gameOver == false)
+        {
+            transform.Translate(Vector3.left * (Time.deltaTime * speed));
+        }
     }
 }
